d90ae92ddef67aa1aa90e8b16c9198717b4cff9f
GTMacieira <macieira.gtalpo@gmail.com>
2022-10-04 22:17:28 -0300

    Criação de objeto, chamda de função

--- ytb_dowloader.py
+++ ytb_dowloader.py
@@ -1,5 +1,9 @@
+from http.client import ImproperConnectionState
 from pytube import YouTube
-import screen_ytb_dowloader as s_ytb
+from screen_ytb_dowloader import Ui_ytb_dowloader
+from PySide6.QtWidgets import (QApplication, QMainWindow, QMessageBox, QTableWidgetItem, QWidget)
+import sys
+
 
 # Pergunta pelo link do YouTube
 
@@ -8,7 +12,6 @@ def download(link):
     yt = YouTube(link)
 
     # Mostra detalhes do vídeo
-    s_ytb.Ui_ytb_dowloader
     print("Título: ",yt.title)
     print("Tempo do vídeo: ",yt.length, " segundos")
     print("Tamando do vídeo: ",yt.streams.get_highest_resolution().filesize ,"em bytes")
@@ -20,3 +23,24 @@ def download(link):
     print("Downloading...")
     ys.download()
     print("Download finalizado!!")
+
+class download_video(Ui_ytb_dowloader, QWidget, YouTube):
+    def __init__(self):
+        super(download_video, self).__init__()
+        self.setupUi(self)
+        
+        self.baixar.clicked.connect(lambda: self.baixar(self.pesq_link.text()))
+
+    def baixar(self,lk):
+        self.dt_title = YouTube.title
+        self.dt_temp = YouTube.length
+        self.dt_tam = YouTube.streams.get_highest_resolution().filesize
+        self.dt_img = YouTube.thumbnail_url
+
+        YouTube.download()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = download_video()
+    window.show()
+    app.exec()
